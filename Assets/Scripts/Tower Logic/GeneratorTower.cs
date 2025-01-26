150b87df4c8a64713f6040148f0b906a04f59d23
grin0021 <55245260+grin0021@users.noreply.github.com>
2025-01-25 23:58:22 -0500

    Create GeneratorTower.cs
    Created a tower variant that adds currency to the player from an external call of the GenerateCurrency function. Currently called at the end of an enemy wave in EnemySpawner script

---
+++ Assets/Scripts/Tower Logic/GeneratorTower.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratorTower : BaseTower
+{
+    [SerializeField] int currencyGeneration = 10;
+    PlayerController player;
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+
+        player = GameObject.Find("player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    protected override void Action()
+    {
+
+    }
+
+    public void GenerateCurrency()
+    {
+        if (GetComponent<DragAndDrop>().enabled == false)
+        {
+            if (player)
+            {
+                player.SetCurrency(player.GetCurrency() + currencyGeneration);
+                Debug.Log("Currency awarded");
+            }
+        }
+    }
+}
