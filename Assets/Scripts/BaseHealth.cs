90ffe234343b75be475de475413659e2d508c8c1
MilesHuschilt <124914551+MilesHuschilt@users.noreply.github.com>
2025-01-26 12:00:37 -0500

    germs and soap

--- Assets/Scripts/BaseHealth.cs
+++ Assets/Scripts/BaseHealth.cs
@@ -12,13 +12,40 @@ public class BaseHealth : MonoBehaviour
     [SerializeField] public int health;
     [SerializeField] public int maxhealth = 100;
     private bool isDead = false;
+
+    public UnityEngine.UI.Image Soap_1;
+    public UnityEngine.UI.Image Soap_2;
+    public UnityEngine.UI.Image Soap_3;
+    public UnityEngine.UI.Image Soap_4;
+
     void Start()
     {
         health = maxhealth;
+        Soap_1.gameObject.SetActive(true);
+        Soap_2.gameObject.SetActive(false);
+        Soap_3.gameObject.SetActive(false);
+        Soap_4.gameObject.SetActive(false);
     }
 
     void Update()
     {
+        if (health > 75 && health <= 100)
+        {
+            Soap_1.gameObject.SetActive(false);
+            Soap_2.gameObject.SetActive(true);
+        }
+        if (health > 50 && health <= 75)
+        {
+            Soap_2.gameObject.SetActive(false);
+            Soap_3.gameObject.SetActive(true);
+        }
+        if (health > 25 && health <= 50)
+        {
+            Soap_3.gameObject.SetActive(false);
+            Soap_4.gameObject.SetActive(true);
+        }
+
+
         if (health <= 0 && !isDead)
         {
             isDead = true;
